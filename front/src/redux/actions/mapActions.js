26e66b0ab63e5fbace645454973f631104afa045
EvBurdin <burdin.e.iddqd@gmail.com>
2019-10-06 13:24:13 +0300

    v1

--- front/src/redux/actions/mapActions.js
+++ front/src/redux/actions/mapActions.js
@@ -1,1 +1,8 @@
-import { CHANGE_SELF_LOCATION, CHANGE_FAMILY_LOCATION } from '../actionNames/mapActionNames';
+import { CHANGE_SELF_LOCATION, GET_FAMILY_LOCATION } from '../actionNames/mapActionNames';
+
+export const getFamilyLocations=()=>async (dispatch)=>{
+    const response = await fetch('http://134.209.82.36.nip.io:3000/api/?familyID=qwerty') //familyID  по идее приходит при логине и хранится в стейте юзера
+    const myJson = await response.json();
+    
+    dispatch({type:GET_FAMILY_LOCATION,payload:myJson});
+}
--- front/src/redux/reducers/mapReducer.js
+++ front/src/redux/reducers/mapReducer.js
@@ -1,4 +1,4 @@
-import { CHANGE_SELF_LOCATION, CHANGE_FAMILY_LOCATION } from '../actionNames/mapActionNames';
+import { CHANGE_SELF_LOCATION, GET_FAMILY_LOCATION } from '../actionNames/mapActionNames';
 
 const initState = {
   selfGPSLocation: '',
@@ -9,7 +9,7 @@ export default function reducer(state = initState, action) {
   switch (action.type) {
     case CHANGE_SELF_LOCATION:
       return { ...state, selfGPSLocation: action.payload };
-    case CHANGE_FAMILY_LOCATION:
+    case GET_FAMILY_LOCATION:
       return { ...state, familyGPSLocation: action.payload };
     default:
       return state;
