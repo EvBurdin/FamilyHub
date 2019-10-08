15c1f4f27c2cde2b69b49568cb2eb73b60ab5ecd
Maklay <okunev.js@gmail.com>
2019-10-08 19:41:08 +0300

    socket test 2

--- backend/start.js
+++ backend/start.js
@@ -5,6 +5,9 @@ global.io = require('socket.io')(server);
 global.io.on('connection', (socket) => {
   console.log(`==========${socket.id}=============`);
   socket.emit('connect');
+  socket.on('hi', () => {
+    socket.emit('hi too');
+  });
 });
 
 server.listen(3000);
