652bca9e2455196dc26609b6b51b9076f343c0f7
Maklay <okunev.js@gmail.com>
2019-10-10 20:59:34 +0300

    change redirect

--- backend/server.js
+++ backend/server.js
@@ -83,7 +83,7 @@ passport.use(db.User.createStrategy());
 
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
   console.log(res.req.cookies['connect.sid']);
-  res.redirect(`https://auth.expo.io/@yok558/family-hub?cookies=${encodeURI(res.req.cookies['connect.sid'])}`);
+  res.redirect(`family-hub://`);
 });
 passport.serializeUser(serializeUser);
 
