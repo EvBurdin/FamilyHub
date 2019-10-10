f782c84265dbf0bd1e08b3315055dbd10c97abf7
Maklay <okunev.js@gmail.com>
2019-10-10 18:10:25 +0300

    add chulk

--- backend/routes/api/regLogin/regLogin.js
+++ backend/routes/api/regLogin/regLogin.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const express = require('express');
 const auth = require('../../../helpers/auth');
+const chalk = require('chalk');
 
 const router = express.Router();
 const controller = require('./controller');
@@ -10,6 +11,13 @@ router.post('/login', passport.authenticate('local'), controller.userLogin);
 router.get('/logged', auth, controller.getCurrentUser);
 // строчка чтоб обновить токены
 // { accessType: 'offline', prompt: 'consent' }
-router.get('/login/google', passport.authenticate('google', { accessType: 'offline', prompt: 'consent' }));
+router.get(
+  '/login/google',
+  (req, res, next) => {
+    console.log(chalk.green(req.cookies));
+    next();
+  },
+  passport.authenticate('google', { accessType: 'offline', prompt: 'consent' }),
+);
 router.get('/logout', auth, controller.userLogout);
 module.exports = router;
