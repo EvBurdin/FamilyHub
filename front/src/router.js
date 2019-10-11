4f73c44df7261c8d1b51e681ccafc66a4481a73f
EvBurdin <burdin.e.iddqd@gmail.com>
2019-10-11 13:14:55 +0300

    Rework AddNewZone + add Logo to router

--- front/src/router.js
+++ front/src/router.js
@@ -2,7 +2,7 @@ import { createSwitchNavigator } from 'react-navigation';
 import React from 'react';
 import { createDrawerNavigator, DrawerNavigatorItems } from 'react-navigation-drawer';
 import {
- View, StyleSheet, ScrollView, Button, SafeAreaView, Dimensions 
+ View, StyleSheet, ScrollView, Text, SafeAreaView, Dimensions 
 } from 'react-native';
 import Main from './views/main';
 import Calendar from './views/Calendar';
@@ -13,8 +13,42 @@ import AddNewZone from './views/AddNewZone';
 import FamilyCreateJoin from './views/familyCreateJoin';
 import ToDo from './views/ToDoList';
 
+const styles = StyleSheet.create({
+  titleContainer: {
+    height: 150,
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+  },
+});
+
 const CustomDrawerComponent = (props) => (
   <ScrollView style={{ marginTop: 30 }}>
+    <View style={styles.titleContainer}>
+      <View style={{ flexDirection: 'row' }}>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 40,
+            backgroundColor: 'black',
+            borderRadius: 5,
+          }}
+        >
+          Family
+        </Text>
+      </View>
+      <View style={{ marginTop: -2, marginLeft: 70 }}>
+        <Text
+          style={{
+            color: 'black',
+            fontSize: 40,
+            backgroundColor: '#FFFF33',
+            borderRadius: 5,
+          }}
+        >
+          Hub
+        </Text>
+      </View>
+    </View>
     <DrawerNavigatorItems {...props} />
   </ScrollView>
 );
